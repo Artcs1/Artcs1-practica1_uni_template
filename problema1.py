bc6ce2415baffb83c6560fb19d00f1f93bd8eca2
Artcs <ju.jeffri.v@gmail.com>
2024-06-04 13:37:42 -0500

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -7,19 +7,4 @@ Created on Wed Feb  7 14:44:11 2024
 
 import math
 n = int(input()) # leer un entero N
-es_primo    = 1
 
-if n!=2 and n%2==0:
-    es_primo = 0
-
-i = 3
-while i <= math.sqrt(n) and es_primo: #O(sqrt(n)/2)
-    if n % i == 0:
-        es_primo = 0
-    i+=2
-  
-
-if es_primo:
-  print('Es primo')
-else:
-  print('No es primo')
