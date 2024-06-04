8965286052f1b0460f3127476ab98709a635db8e
Artcs <ju.jeffri.v@gmail.com>
2024-06-04 13:38:28 -0500

    Update problema5.py

--- problema5.py
+++ problema5.py
@@ -7,16 +7,3 @@ Created on Wed Feb  7 14:44:13 2024
 
 import math
 n = int(input()) # leer un entero N
-
-
-suma = 0
-for i in range(1,int(math.sqrt(n))+1):
-    if n%i == 0:
-        suma+=i
-        if n//i != n:
-            suma+=n//i
-            
-if suma == n:
-    print('Es perfecto')
-else:
-    print('No es perfecto')
