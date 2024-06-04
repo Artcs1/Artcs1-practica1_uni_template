6c450f20cb586f5dfa82fc76441fa998e6ac12e1
Artcs <ju.jeffri.v@gmail.com>
2024-06-04 13:38:07 -0500

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -12,16 +12,3 @@ BC = round(float(input()),2)
 CD = round(float(input()),2)
 DA = round(float(input()),2)
 AC = round(float(input()),2)
-
-if AB == BC and BC == CD and CD == DA:
-    if round(math.sqrt(round(AB**2,2) + round(BC**2,2,)),2) == AC:
-        print('Es un cuadrado')
-    else:
-        print('Es un rombo')
-elif AB == CD and DA == BC:
-    if round(math.sqrt(round(AB**2,2) + round(BC**2,2,)),2) == AC:
-        print('Es un rectangulo')
-    else:
-        print('Es un paralelogramo')
-else:
-    print('No existe')
